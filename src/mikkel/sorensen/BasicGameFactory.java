61392bf7104fe0435fcf8ab4ff24685dab417f4c
Mikkel Sørensen <mik4211@edu.easj.dk>
2018-01-30 12:41:02 +0100

    Tilføjet player, blå firkant. Ændres senere. Playeren kan bevæge sig rundt på platformen.

--- src/mikkel/sorensen/BasicGameFactory.java
+++ src/mikkel/sorensen/BasicGameFactory.java
@@ -4,9 +4,10 @@ import com.almasb.fxgl.entity.*;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-
+import javafx.scene.shape.Rectangle;
 
 
 @SetEntityFactory
@@ -18,6 +19,19 @@ public class BasicGameFactory implements EntityFactory{
                 .from(data)
                 .bbox(new HitBox(BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
                 .with(new PhysicsComponent())
+                .build();
+    }
+    @Spawns("player")     //Generere spillerens karakter
+    public Entity newPlayer(SpawnData data) {
+
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.DYNAMIC);
+
+        return Entities.builder()
+                .from(data)
+                .viewFromNodeWithBBox(new Rectangle(30, 30, Color.BLUE))
+                .with(physics)
+                .with(new PlayerControl())
                 .build();
     }
     @Spawns("coin")
